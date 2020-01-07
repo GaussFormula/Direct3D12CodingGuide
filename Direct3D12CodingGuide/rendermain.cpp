2faf99119c573a3bf1ec016930523676d7cdcbe5
GaussFormula <c594794@hotmail.com>
2020-01-07 23:25:08 +0800

    Add CreateDXGIFactory

--- Direct3D12CodingGuide/rendermain.cpp
+++ Direct3D12CodingGuide/rendermain.cpp
@@ -8,6 +8,23 @@ DWORD WINAPI RenderThreadMain(LPVOID lpThreadParameter)
 {
     HWND hWnd = static_cast<HWND>(lpThreadParameter);// The handle of window
 
+    // A pointer for DXGIFactory
+    IDXGIFactory* pDXGIFactory;
+    CreateDXGIFactory(IID_PPV_ARGS(&pDXGIFactory));
+    ID3D12Device* pD3D12Device = NULL;
+    {
+        IDXGIAdapter* pIDXGIAdapter;
+        // Transversal all the adapter to try 
+        for (UINT i = 0U; SUCCEEDED(pDXGIFactory->EnumAdapters(i, &pIDXGIAdapter)); ++i)
+        {
+            if (SUCCEEDED(D3D12CreateDevice(pIDXGIAdapter, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&pD3D12Device))))
+            {
+                pIDXGIAdapter->Release();// You can release the adapter after create device
+                break;
+            }
+            pIDXGIAdapter->Release();
+        }
+    }
 
     return 0U;
 }
